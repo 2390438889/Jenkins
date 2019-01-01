c43787033eabcaddb66b0d6352a5a51b67cf23d4
2390438889 <2390438889@qq.com>
2019-01-01 18:49:46 +0800

    change Hello.java RequestMapping("/hello")

--- src/main/java/com/jenkins/lujie/controller/Hello.java
+++ src/main/java/com/jenkins/lujie/controller/Hello.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @ResponseBody
 public class Hello {
-    @RequestMapping("/hello")
+    @RequestMapping("/hi")
     public String hello(){
         return "hello";
     }
