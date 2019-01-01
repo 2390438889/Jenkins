f27e504551ee80ffe5389dd4af419a942f5969ff
2390438889 <2390438889@qq.com>
2019-01-01 13:13:21 +0800

    11

---
+++ src/main/java/com/jenkins/lujie/controller/Hello.java
@@ -0,0 +1,23 @@
+package com.jenkins.lujie.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/*
+ *=================================================================================================  
+ * 日期		             开发者	       修改类型  	      备注	
+ * 2018/11/28			 Hearts         创建	          描述：请输入描述
+ *================================================================================================= 
+ *  Copyright (c) 2018-2020 湖南创博龙智信息科技股份有限公司 All rights reserved.
+ *================================================================================================= 
+ */
+@Controller
+@ResponseBody
+public class Hello {
+    @RequestMapping("/")
+    public String hello(){
+        return "hello";
+    }
+
+}
