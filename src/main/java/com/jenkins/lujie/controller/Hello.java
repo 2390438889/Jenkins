44e1a679335891cb1b8a245444050555604e3c5b
2390438889 <2390438889@qq.com>
2019-01-02 08:46:30 +0800

    test jenkins auto update

--- src/main/java/com/jenkins/lujie/controller/Hello.java
+++ src/main/java/com/jenkins/lujie/controller/Hello.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class Hello {
     @RequestMapping("/hello")
     public String hello(){
-        return "12345";
+        return "1234578";
     }
 
 }
